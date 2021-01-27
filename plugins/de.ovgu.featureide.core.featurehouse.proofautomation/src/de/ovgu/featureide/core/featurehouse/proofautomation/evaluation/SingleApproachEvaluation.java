cef3ba9ff53fc04d4f85973825ab0d4326071a86
Alexander Knueppel <a.knueppel@tu-bs.de>
2021-01-27 09:50:35 +0100

    Just formatting stuff

--- plugins/de.ovgu.featureide.core.featurehouse.proofautomation/src/de/ovgu/featureide/core/featurehouse/proofautomation/evaluation/SingleApproachEvaluation.java
+++ plugins/de.ovgu.featureide.core.featurehouse.proofautomation/src/de/ovgu/featureide/core/featurehouse/proofautomation/evaluation/SingleApproachEvaluation.java
@@ -41,99 +41,118 @@ import de.ovgu.featureide.core.featurehouse.proofautomation.verification.MethodI
 import de.ovgu.featureide.core.featurehouse.proofautomation.verification.ThuemEtAl;
 import de.ovgu.featureide.core.featurehouse.proofautomation.verification.ThuemEtAlReuse;
 
-
 /**
  * Represents a Evaluation over one approach
  * 
  * @author Marlen Herter-Bernier
  */
-public class SingleApproachEvaluation extends Evaluation{
+public class SingleApproachEvaluation extends Evaluation {
 	public int failedProofs = 0;
 	public int proofs = 0;
 	private int evalVersion;
 	private List<ProofInformation> phase1ProofList = new LinkedList<ProofInformation>();
-	private List<ProofInformation> proofList = new LinkedList<ProofInformation>(); //contains all Automating proofs of this project
+	private List<ProofInformation> proofList = new LinkedList<ProofInformation>(); // contains all Automating proofs of
+																					// this project
 	private List<ProofInformation> proofList1And2Phase = new LinkedList<ProofInformation>();
-public static void main(String[] args) {
-	SingleApproachEvaluation s = new SingleApproachEvaluation(new File("/mnt/54AFF99F466B2AED/Informatik/Masterarbeit/AbstractExecution/account/BankAccountv1"), 1, 
-			"/mnt/54AFF99F466B2AED/Informatik/Masterarbeit/AbstractExecution/account/Evaluation/BankAccountv1", "AbstractContract");
-	s.performEvaluation();
-}
+
+	public static void main(String[] args) {
+		SingleApproachEvaluation s = new SingleApproachEvaluation(
+				new File("/mnt/54AFF99F466B2AED/Informatik/Masterarbeit/AbstractExecution/account/BankAccountv1"), 1,
+				"/mnt/54AFF99F466B2AED/Informatik/Masterarbeit/AbstractExecution/account/Evaluation/BankAccountv1",
+				"AbstractContract");
+		s.performEvaluation();
+	}
 
 	/**
-	 * Constructor for execution in an verification approach
-	 * Uses a given evaluatePath instead of a new
+	 * Constructor for execution in an verification approach Uses a given
+	 * evaluatePath instead of a new
+	 * 
 	 * @param f
 	 * @param evalVersion
 	 * @param evaluatePath
 	 */
-	public SingleApproachEvaluation(File f, int evalVersion, String evaluatePath, String method){
+	public SingleApproachEvaluation(File f, int evalVersion, String evaluatePath, String method) {
 		super(f);
 		System.out.println("Init SingleApproachEvaluation with Method " + method + "with Eval Version " + evalVersion);
 		this.method = method;
 		this.evaluatePath = FileManager.createDir(new File(evaluatePath));
-		if(evalVersion>0){
+		if (evalVersion > 0) {
 			this.evalVersion = evalVersion;
-		}
-		else{
+		} else {
 			this.evalVersion = getApproachVersion();
 		}
-		this.statistics = new File (evaluatePath+FILE_SEPERATOR+"Evaluation Results-VA" + this.evalVersion+".xlsx");
+		this.statistics = new File(
+				evaluatePath + FILE_SEPERATOR + "Evaluation Results-VA" + this.evalVersion + ".xlsx");
 	}
 
-	/* (non-Javadoc)
-	 * @see de.ovgu.featureide.core.featurehouse.proofautomation.evaluation.Evaluation#performEvaluation()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.ovgu.featureide.core.featurehouse.proofautomation.evaluation.Evaluation#
+	 * performEvaluation()
 	 */
 	@Override
 	public void performEvaluation() {
-		if(evalVersion == 5||evalVersion == 6){
+		if (evalVersion == 5 || evalVersion == 6) {
 			Configuration.setCurrentMetaproductwithDispatcher(false);
-		}else{
+		} else {
 			Configuration.setCurrentMetaproductwithDispatcher(true);
 		}
 		FileManager.initFolders(evaluatePath, evalVersion);
-		if(evalVersion == 1){
+		if (evalVersion == 1) {
 			
-			File fstubPath = new File(toEvaluate.getAbsolutePath()+FILE_SEPERATOR+FileManager.featureStubDir);
-			//Fix here instead of in performVa1 (i.e., before copy)
-			File transactionAccount = new File(toEvaluate.getAbsolutePath()+FILE_SEPERATOR+FileManager.featureStubDir+FILE_SEPERATOR+"Transaction"+FILE_SEPERATOR+"Account.java");
-			File lockAccount = new File(toEvaluate.getAbsolutePath()+FILE_SEPERATOR+FileManager.featureStubDir+FILE_SEPERATOR+"Lock"+FILE_SEPERATOR+"Account.java");
-			FeatureStubBuilder.prepareForVerification(transactionAccount,lockAccount);
+			
 
-			
-			//copy
-			FileManager.copyCompleteFolderContent(fstubPath,new File(evaluatePath.getAbsolutePath()+FILE_SEPERATOR+FileManager.featureStubDir));
+			File fstubPath = new File(toEvaluate.getAbsolutePath() + FILE_SEPERATOR + FileManager.featureStubDir);
+			// Fix here instead of in performVa1 (i.e., before copy)
+			File transactionAccount = new File(toEvaluate.getAbsolutePath() + FILE_SEPERATOR
+					+ FileManager.featureStubDir + FILE_SEPERATOR + "Transaction" + FILE_SEPERATOR + "Account.java");
+			File lockAccount = new File(toEvaluate.getAbsolutePath() + FILE_SEPERATOR + FileManager.featureStubDir
+					+ FILE_SEPERATOR + "Lock" + FILE_SEPERATOR + "Account.java");
+			FeatureStubBuilder.prepareForVerification(transactionAccount, lockAccount);
+
+			// copy
+			FileManager.copyCompleteFolderContent(fstubPath,
+					new File(evaluatePath.getAbsolutePath() + FILE_SEPERATOR + FileManager.featureStubDir));
 
 		}
-		
-		File metaproductPath = new File(toEvaluate.getAbsolutePath()+FILE_SEPERATOR+FileManager.metaproductDir);
-		FileManager.copyCompleteFolderContent(metaproductPath,new File(evaluatePath.getAbsolutePath()+FILE_SEPERATOR+FileManager.metaproductDir));
+
+		File metaproductPath = new File(toEvaluate.getAbsolutePath() + FILE_SEPERATOR + FileManager.metaproductDir);
+		FileManager.copyCompleteFolderContent(metaproductPath,
+				new File(evaluatePath.getAbsolutePath() + FILE_SEPERATOR + FileManager.metaproductDir));
 		AbstractVerification abstractVerification = null;
-		switch(evalVersion){
-		case 1 :abstractVerification = FefalutionFamilyProofReplay.getInstance();
-				break;				
-		case 2 :abstractVerification = Metaproduct.getInstance();
-				break;
-		case 3 :abstractVerification = ConcreteContracts.getInstance();
-				break;
-		case 4 :abstractVerification = MethodInling.getInstance();
-				break;
-		case 5 :abstractVerification = ThuemEtAl.getInstance();
-				break;
-		case 6 :abstractVerification = ThuemEtAlReuse.getInstance();
-				break;
+		switch (evalVersion) {
+		case 1:
+			abstractVerification = FefalutionFamilyProofReplay.getInstance();
+			break;
+		case 2:
+			abstractVerification = Metaproduct.getInstance();
+			break;
+		case 3:
+			abstractVerification = ConcreteContracts.getInstance();
+			break;
+		case 4:
+			abstractVerification = MethodInling.getInstance();
+			break;
+		case 5:
+			abstractVerification = ThuemEtAl.getInstance();
+			break;
+		case 6:
+			abstractVerification = ThuemEtAlReuse.getInstance();
+			break;
 		}
 
-		if( method.equals("Non Rigid")) {
+		if (method.equals("Non Rigid")) {
 			System.out.println("Starte Proof with Abstract Contracts");
 			abstractVerification.keyHandler = new Non_Rigid();
-		}else if(method.equals("DefaultKeY")) {
+		} else if (method.equals("DefaultKeY")) {
 			System.out.println("Starte Proof with DefaultKeY");
 			abstractVerification.keyHandler = new DefaultKeY();
 		}
 		abstractVerification.warmUp(FileManager.getFirstMetaproductElement(toEvaluate), method);
-		abstractVerification.performVerification(toEvaluate,evaluatePath);
-		
+		abstractVerification.performVerification(toEvaluate, evaluatePath);
+
 		proofList = abstractVerification.getProofList();
 		phase1ProofList = abstractVerification.getPhase1ProofList();
 		proofList1And2Phase = abstractVerification.getProofListWithPhase1And2();
@@ -145,36 +164,43 @@ public static void main(String[] args) {
 		abstractVerification.setphase1ProofList(new LinkedList<ProofHandler>());
 	}
 
-	/* (non-Javadoc)
-	 * @see de.ovgu.featureide.core.featurehouse.proofautomation.evaluation.Evaluation#createXLS()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * de.ovgu.featureide.core.featurehouse.proofautomation.evaluation.Evaluation#
+	 * createXLS()
 	 */
 	@Override
 	public void createXLS() {
 		ExcelManager2.generateSingleProjectWithReuseXLS(this);
-		
+
 	}
+
 	/**
 	 * Returns the approach version according to the directory
+	 * 
 	 * @return
 	 */
-	private int getApproachVersion(){
+	private int getApproachVersion() {
 		String name = evaluatePath.getParentFile().getName();
-		for(int i = 1; i<=15; i++){
+		for (int i = 1; i <= 15; i++) {
 			String number = String.valueOf(i);
-			if(name.contains(number)){
+			if (name.contains(number)) {
 				return i;
 			}
 		}
 		return 0;
 	}
-	
+
 	/**
 	 * 
 	 * @return proofList
 	 */
-	public List<ProofInformation> getProofList(){
+	public List<ProofInformation> getProofList() {
 		return proofList;
 	}
+
 	/**
 	 * @return the phase1ProofList
 	 */
@@ -188,62 +214,65 @@ public static void main(String[] args) {
 	public List<ProofInformation> getProofList1And2Phase() {
 		return proofList1And2Phase;
 	}
+
 	/**
 	 * removes the statistic of a give proof in the phase1ProofList and proofList
+	 * 
 	 * @param target
 	 * @param type
 	 */
-	public void removeProofFromSum(String target, String type){
-		if(phase1ProofList != null){
-			for(ProofInformation ap : phase1ProofList){
-				if(ap.getTargetName().equals(target)&&ap.getTypeName().equals(type)){
+	public void removeProofFromSum(String target, String type) {
+		if (phase1ProofList != null) {
+			for (ProofInformation ap : phase1ProofList) {
+				if (ap.getTargetName().equals(target) && ap.getTypeName().equals(type)) {
 					firstPhase.removeStatistics(ap.getStat());
 					firstPhaseReuse.removeStatistics(ap.getReusedStat());
 				}
 			}
 		}
-		for(ProofInformation ap : proofList){
-			if(ap.getTargetName().equals(target)&&ap.getTypeName().equals(type)){
+		for (ProofInformation ap : proofList) {
+			if (ap.getTargetName().equals(target) && ap.getTypeName().equals(type)) {
 				secondPhase.removeStatistics(ap.getStat());
 				secondPhaseReuse.removeStatistics(ap.getReusedStat());
 			}
 		}
 	}
-	
+
 	/**
 	 * updates the list of failed proofs
 	 */
-	public void updateFailedProofs(){
-		for(ProofInformation ap : proofList){
-			if(!ap.isClosed()){
+	public void updateFailedProofs() {
+		for (ProofInformation ap : proofList) {
+			if (!ap.isClosed()) {
 				failedProofs++;
 			}
 		}
 	}
-	
+
 	/**
-	 * Updates the Statistics 
+	 * Updates the Statistics
 	 */
-	public void updateSum(){
+	public void updateSum() {
 		firstPhase.reset();
 		firstPhaseReuse.reset();
 		secondPhase.reset();
 		secondPhaseReuse.reset();
-		if(phase1ProofList != null){
-			for(ProofInformation ap : phase1ProofList){
+		if (phase1ProofList != null) {
+			for (ProofInformation ap : phase1ProofList) {
 				firstPhase.addStatistics(ap.getStat());
 				firstPhaseReuse.addStatistics(ap.getReusedStat());
 			}
 		}
-		for(ProofInformation ap : proofList){
+		for (ProofInformation ap : proofList) {
 			secondPhase.addStatistics(ap.getStat());
 			secondPhaseReuse.addStatistics(ap.getReusedStat());
 		}
 	}
+
 	/**
 	 * updates the number of Proofs
 	 */
-	public void updateProofCount(){
+	public void updateProofCount() {
 		proofs += proofList.size();
 	}
 }
